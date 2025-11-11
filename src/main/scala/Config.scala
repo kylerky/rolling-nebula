a9449197825cafc928717a56c06f4a4c258d0f96
Smart Goose <kylerky@live.com>
2025-11-11 22:49:57 +0800

    refactor: Update ConfigLoader to return IO

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -1,14 +1,14 @@
 package com.nebula.rolling
 
+import cats.effect.IO
 import io.circe.generic.auto._
 import io.circe.config.parser
-import scala.util.Try
 
 case class HostConfig(name: String, ip: String, groups: List[String])
 case class AppConfig(hosts: Map[String, HostConfig])
 
 object ConfigLoader {
-  def load(): Try[AppConfig] = {
-    parser.decode[AppConfig]().toTry
+  def load(): IO[AppConfig] = {
+    parser.decodeF[IO, AppConfig]()
   }
 }
